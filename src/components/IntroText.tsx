f22afc534df830144902486700b84015863cc1c1
Filip Danić <filip@spicefactory.co>
2020-05-17 21:22:16 +0200

    Add data for past 2 days

--- src/components/IntroText.tsx
+++ src/components/IntroText.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import WarningAlert from './WarningAlert';
 
-const ConfirmedCases = '10.438';
+const ConfirmedCases = '10.610';
 
 export default () =>
   <React.Fragment>
