6190bf194453440e1d6236e7ca98d24b6816b612
Filip Danić <filip@spicefactory.co>
2020-05-18 19:06:43 +0200

    Add data for 2020/05/18

--- src/components/IntroText.tsx
+++ src/components/IntroText.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import WarningAlert from './WarningAlert';
 
-const ConfirmedCases = '10.610';
+const ConfirmedCases = '10.699';
 
 export default () =>
   <React.Fragment>
